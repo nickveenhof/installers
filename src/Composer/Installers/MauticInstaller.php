fb2a947c5ce4d97d55c57ba456b2dea57e0a0a7a
Nick Veenhof <nick.veenhof@gmail.com>
2021-03-18 14:51:06 +0100

    Updating repo

--- src/Composer/Installers/MauticInstaller.php
+++ src/Composer/Installers/MauticInstaller.php
@@ -1,25 +1,50 @@
 <?php
 namespace Composer\Installers;
 
+use Composer\Package\PackageInterface;
+
 class MauticInstaller extends BaseInstaller
 {
     protected $locations = array(
         'plugin'           => 'plugins/{$name}/',
         'theme'            => 'themes/{$name}/',
         'core'             => 'app/',
-        'custom-theme'     => 'themes/custom/{$name}/',
-        'custom-plugin'    => 'plugins/custom/{$name}/',
     );
+
+    /**
+     * @param PackageInterface $package
+     *
+     * @return string
+     */
+    private function getDirectoryName(PackageInterface $package)
+    {
+        $extra = $package->getExtra();
+
+        if (!empty($extra['install-directory-name'])) {
+            return $extra['install-directory-name'];
+        }
+
+        return $this->toCamelCase($package->getPrettyName());
+    }
+
+    /**
+     * @param string $packageName
+     *
+     * @return string
+     */
+    private function toCamelCase($packageName)
+    {
+        return str_replace(' ', '', ucwords(str_replace('-', ' ', basename($packageName))));
+    }
 
     /**
      * Format package name of mautic-plugins to CamelCase
      */
     public function inflectPackageVars($vars)
     {
+        var_dump($vars);
         if ($vars['type'] == 'mautic-plugin') {
-            $vars['name'] = preg_replace_callback('/(-[a-z])/', function ($matches) {
-                return strtoupper($matches[0][1]);
-            }, ucfirst($vars['name']));
+            $vars['name'] = $this->getDirectoryName($vars['name']);
         }
 
         return $vars;
